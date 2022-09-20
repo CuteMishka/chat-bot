7ad2fe695b6f85e4f1af764423424a8120f8c70a
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-09-20 16:11:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-
+function func() {
+ var message = document.getElementById('input').value;
+ var row = document.createElement("div")
+ var chat = document.getElementById('chat')
+ row.className = "row user";
+ chat.appendChild(row)
+ row.innerHTML = '<div class="bubble"></div>';
+ document.getElementByClassName('bubble').textContent = message;
+}
