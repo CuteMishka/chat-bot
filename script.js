343e295bbbf116dd61be377a6d1d618c79a03095
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-09-20 18:49:08 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
-function func() {
+var chat = document.getElementById('chat');
+function send_user_message() {
  var message = document.getElementById('input').value;
- var row = document.createElement("div")
- var chat = document.getElementById('chat')
+ let row = document.createElement("div")
  row.className = "row user";
- chat.appendChild(row)
- row.innerHTML = '<div class="bubble"></div>';
- document.getElementByClassName('bubble').textContent = message;
+ chat.appendChild(row);
+ row.innerHTML = '<div class="bubble">'+ message +'</div>';
+ AI_response()
+}
+function AI_response(message) {
+	let questions = ["Как тебя зовут?","Как меня зовут?","Ты меня найдёшь?"]
+	let answers = ["Меня зовут Роберт","Тебя зовут никак","Определённо"]
+	let response_found = false
+	let response = "Я вас не понимаю"
+	var message = document.getElementById('input').value;
+	let row = document.createElement("div");
+	row.className = "row robot"
+	chat.appendChild(row);
+	row.innerHTML = '<div class="bubble"> Вы написали ' + message +'</div>';
+	for (let [key,value] of answers){
+		console.log(value)
+		console.log(key)
+	}
 }
