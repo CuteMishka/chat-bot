7f1dcf8f677878233cb8ba38f6eb40d74782a378
CuteMishka <108468031+CuteMishka@users.noreply.github.com>
2022-09-27 16:03:45 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 var chat = document.getElementById('chat');
+let answer;
+let message;
 function send_user_message() {
- var message = document.getElementById('input').value;
+ message = document.getElementById('input').value;
  let row = document.createElement("div")
  row.className = "row user";
  chat.appendChild(row);
  row.innerHTML = '<div class="bubble">'+ message +'</div>';
+  Lgb();
  AI_response()
 }
 function AI_response(message) {
-	let questions = ["Как тебя зовут?","Как меня зовут?","Ты меня найдёшь?"]
-	let answers = ["Меня зовут Роберт","Тебя зовут никак","Определённо"]
-	let response_found = false
-	let response = "Я вас не понимаю"
-	var message = document.getElementById('input').value;
-	let row = document.createElement("div");
-	row.className = "row robot"
-	chat.appendChild(row);
-	row.innerHTML = '<div class="bubble"> Вы написали ' + message +'</div>';
-	for (let [key,value] of answers){
-		console.log(value)
-		console.log(key)
-	}
+ 
+ let response_found = false
+ let response = "Я вас не понимаю"
+ message = document.getElementById('input').value;
+ let row = document.createElement("div");
+ row.className = "row robot"
+ chat.appendChild(row);
+ row.innerHTML = '<div class="bubble"> Вы написали ' + answer +'</div>';
+  
 }
+
+ const Lgb = () => {
+    let questions = [{
+id: 1, 
+messageFind: "Как дела"
+}];
+
+let answers = [{
+id: 1,
+answer: "Нормально"
+}]
+if (message){
+
+let answerId = questions.find(mes => mes.messageFind === message).id;
+answer = answers.find(answerFound => answerFound.id=== answerId).answer;
+}
+  }
